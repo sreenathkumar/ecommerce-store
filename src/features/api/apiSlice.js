190153aac9d64feb8459bcff5198cb0ea9df8173
Sreenath Kumar <60534594+sreenathkumar@users.noreply.github.com>
2023-02-12 06:22:57 +0000

    add prepareHeaders

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,14 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 export const apiSlice = createApi({
    redeucerPath: 'api',
    baseQuery: fetchBaseQuery({
-      baseUrl: process.env.REACT_API_URL
+      baseUrl: process.env.REACT_API_URL,
+      prepareHeaders: async(headers,{getState,endpoints})=>{
+         const token= getState()?.auth?.accessToken;
+         if(token){
+            headers.set("Authorization",`Bearer ${token}`)
+         }
+         return headers
+      }
    }),
    endpoints: (builder) => ({}),
 })
