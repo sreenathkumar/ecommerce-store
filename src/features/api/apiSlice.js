c88c4171392671c4e5ed030f4ccaa6dbf539a6cb
Sreenath <sreenathkumar66@gmail.com>
2023-05-20 18:03:51 +0600

    added remote DB url and finalize add category template

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
    reducerPath: 'api',
    baseQuery: fetchBaseQuery({
-      baseUrl: process.env.REACT_APP_API_URL,
+      baseUrl: process.env.REACT_APP_REMOTE_API_URL || process.env.REACT_APP_API_URL,
       credentials: "include"
    }),
    endpoints: (builder) => ({})
